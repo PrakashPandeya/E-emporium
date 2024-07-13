ef438470b30af1b44fc38207316ad4560d52a272
PrakashPandeya <prakash.07pandeya@gmail.com>
2024-07-13 23:31:58 +0545

    Admin Home  Servlet

---
+++ src/main/java/controller/servlets/AdminHome.java
@@ -0,0 +1,48 @@
+package controller.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.DatabaseController;
+
+/**
+ * Servlet implementation class AdminHome
+ */
+@WebServlet("/AdminHome")
+public class AdminHome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	DatabaseController dbc=new DatabaseController();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminHome() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setAttribute("userCount", dbc.checkCount("user"));
+		request.setAttribute("productCount", dbc.checkCount("product"));
+		request.setAttribute("orderCount", dbc.checkCount("orders"));
+		request.getRequestDispatcher("/pages/admin/dashboard_home.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
