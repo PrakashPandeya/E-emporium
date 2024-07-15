b2c44442dac15986f0b583a5c99081ccb8333449
PrakashPandeya <prakash.07pandeya@gmail.com>
2024-07-15 08:08:32 +0545

    User About

---
+++ src/main/java/controller/servlets/UserAbout.java
@@ -0,0 +1,43 @@
+package controller.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.StringUtils;
+
+/**
+ * Servlet implementation class UserAbout
+ */
+@WebServlet(asyncSupported = true, urlPatterns = {StringUtils.SERVLET_URL_USER_ABOUT})
+public class UserAbout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserAbout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/pages/user/user_about.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
